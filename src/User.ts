55203a93bb2d36308322ca9e45a98a8826c456f5
Fayez Ghazzawi <fayez.ghazzawi@outlook.com>
2023-11-01 16:08:57 +0400

    510 Using Type Definition Files

--- src/User.ts
+++ src/User.ts
@@ -7,5 +7,11 @@ class User {
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.person.firstName();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
 }

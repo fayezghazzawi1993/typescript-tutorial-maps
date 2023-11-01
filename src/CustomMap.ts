fd11a737bdb993ed90877d39c1b7da81e92403d6
Fayez Ghazzawi <fayez.ghazzawi@outlook.com>
2023-11-01 17:08:04 +0400

    521 Restricting Access with Interfaces

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,13 @@
 import { Company } from './Company';
 import { User } from './User';
 
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -18,7 +25,7 @@ export class CustomMap {
     }
   }
 
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {

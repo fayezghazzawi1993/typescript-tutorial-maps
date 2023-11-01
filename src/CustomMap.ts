97be919a6c410c3c5a7b70d84e43b43082a21a71
Fayez Ghazzawi <fayez.ghazzawi@outlook.com>
2023-11-01 17:15:22 +0400

    523 Showing Popup Windows

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -13,7 +13,7 @@ export class CustomMap {
 
     if (mapDiv) {
       this.googleMap = new google.maps.Map(mapDiv, {
-        zoom: 3,
+        zoom: 1,
         center: {
           lat: 0,
           lng: 0,
@@ -23,12 +23,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!',
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
